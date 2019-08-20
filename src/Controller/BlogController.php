50d36e610924a67cd464a5e2154382f3cce07360
frédéric ribot <frederic.ribot.pro@gmail.com>
2019-08-20 17:48:13 +0200

    Le rooting avancé

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,12 +13,30 @@ use Symfony\Component\HttpFoundation\Response;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/Blog", name="Blog_index")
+     * @Route("/Blog/show", name="Blog_index")
      */
+
     public function index()
     {
         return $this->render('index.html.twig', [
             'owner' => 'Thomas',
         ]);
     }
+      /**
+     * @Route("/blog/show/{page}",
+     * requirements={"page"="[a-z0-9\-]+"},
+     * defaults = {"page":"mon-super-article"},
+     * name="blog_show")
+     */
+
+    public function show($page)
+    {
+       $page = ucwords(str_replace('-',' ',$page));
+        
+        return $this->render('show.html.twig' , ['page'=> $page]);
+
+    
+    }
+
+
 }
